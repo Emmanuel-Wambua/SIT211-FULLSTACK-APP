9145d6e76bd75ad6521dbc2ac88431b1c9ec75f8
Emmanuel Wambua <mantelmanu31@gmail.com>
2026-05-20 23:14:56 +0300

    changed mailing system

--- wambuxtore_backend_fixed/wambuxtore_backend/settings.py
+++ wambuxtore_backend_fixed/wambuxtore_backend/settings.py
@@ -143,13 +143,13 @@ ANYMAIL = {
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+# DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
